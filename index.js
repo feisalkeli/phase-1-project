1828a8e36c20750e70c7538e5bbb56d475f93445
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-04-05 06:57:52 +0300

    implemented remove item

--- index.js
+++ index.js
@@ -9,23 +9,44 @@ function appendData(products) {
   card.className = "data";
   card.innerHTML = `
  
-  <div class="card" style="width: 18rem;">
+  <div class="card" style="width: 18rem;" id="content">
   <img src=${products.thumbnail} class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${products.title}</h5>
     <p class="card-text">${products.description}</p>
-    <button  class="btn btn-primary">Add to Cart</button>
-     <button  class="btn btn-primary"> Remove Item</button>
+    <button id="info-button" class="btn btn-primary">Display Info</button>
+     <button id="remove-item" class="btn btn-primary"> Remove Item</button>
+
   </div>
 </div>
 
   `;
+  ///Selecting the remove-item button info button
+  card.querySelector("#remove-item").addEventListener("click", (e) => {
+    card.innerHTML = "";
+    deleteCards(products.id);
+  });
+  // Selecting and showing the details of a card
+  card.querySelector("#info-button").addEventListener("click", (e) => {
+    console.log(e);
+  });
+
+  //Add products card to the DOM
   document.querySelector("#main").append(card);
 }
-
-function clickOnProduct() {}
+///Delete data in the Dom and also json
+function deleteCards(id) {
+  fetch(`http://localhost:3000/products/${id}`, {
+    method: `DELETE`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
 
 function initializeData() {
-  fetchProducts();
+  window.addEventListener("DOMContentLoaded", fetchProducts);
 }
 initializeData();
